b8fd15e8832cb150f8e7205f75f4e89d571e7365
Hadas slavin <hadasfeldi@gmail.com>
2020-03-24 16:31:57 +0200

    add create anagram map function

--- src/main/java/anagram/services/Main.java
+++ src/main/java/anagram/services/Main.java
@@ -4,23 +4,51 @@ import org.glassfish.grizzly.http.server.HttpServer;
 import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
 import org.glassfish.jersey.server.ResourceConfig;
 
+import java.io.File;
 import java.io.IOException;
 import java.net.URI;
+import java.util.*;
 
 public class Main {
     public static final String BASE_URI = "http://localhost:8080/myapp/";
+    public static final String WORDS_DB_PATH = Main.class.getClassLoader().getResource("words_clean.txt").getPath();
+    public static Map<String, List<String>> anagramMap = new HashMap<>();
 
-        public static HttpServer startServer() {
-            final ResourceConfig rc = new ResourceConfig().packages("anagram.services");
+    public static void CreateAnagramMap() {
+        try (Scanner scanner = new Scanner(new File(WORDS_DB_PATH))) {
 
-            return GrizzlyHttpServerFactory.createHttpServer(URI.create(BASE_URI), rc);
+            while (scanner.hasNext()) {
+                String sourceWord = scanner.nextLine();
+                char[] wordChars = sourceWord.toCharArray();
+                Arrays.sort(wordChars);
+                String sortWord = new String(wordChars);
+
+                if (anagramMap.containsKey(sortWord)) {
+                    anagramMap.get(sortWord).add(sourceWord);
+                } else {
+                    List<String> sourceWordList = new ArrayList<>();
+                    sourceWordList.add(sortWord);
+                    anagramMap.put(sortWord, sourceWordList);
+                }
+            }
+
+        } catch (IOException e) {
+            System.out.println("could'nt load " + WORDS_DB_PATH +  "file. error: " + e.getMessage());
         }
+    }
 
-        public static void main(String[] args) throws IOException {
-            final HttpServer server = startServer();
-            System.out.println(String.format("The Anagram app started! available at {} " +
-                    "Hit enter to stop it...", BASE_URI));
-            System.in.read();
-            //server.stop();
-        }
+    public static HttpServer startServer() {
+        final ResourceConfig rc = new ResourceConfig().packages("anagram.services");
+
+        CreateAnagramMap();
+        return GrizzlyHttpServerFactory.createHttpServer(URI.create(BASE_URI), rc);
+    }
+
+    public static void main(String[] args) throws IOException {
+        final HttpServer server = startServer();
+        System.out.println(String.format("The Anagram app started! available at {} " +
+                "Hit enter to stop it...", BASE_URI));
+        System.in.read();
+        //server.stop();
+    }
 }
