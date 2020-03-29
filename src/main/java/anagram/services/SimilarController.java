266709b7cfc2177f742b47aa34ac01cb9c94ae40
Hadas slavin <hadasfeldi@gmail.com>
2020-03-29 22:11:18 +0300

    fix remove similar bug

--- src/main/java/anagram/services/SimilarController.java
+++ src/main/java/anagram/services/SimilarController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 @RestController
@@ -30,7 +31,7 @@ public class SimilarController {
             List<String> similarWordsList = new ArrayList();
 
             if (AnagramManager.getAnagramMap().containsKey(sortWord)) {
-                similarWordsList = AnagramManager.getAnagramMap().get(sortWord);
+                similarWordsList.addAll(AnagramManager.getAnagramMap().get(sortWord));
                 similarWordsList.remove(word);
             }
 
